6bd75fbca65fd53e0163d691d14b4325f590f02f
Razuev Anton <Raazuev@gmail.com>
2025-01-21 22:58:00 +0500

    api

---
+++ src/components/api/heroes.js
@@ -0,0 +1,19 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+const filePath = join(process.cwd(), 'heroes.json');
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    const data = JSON.parse(readFileSync(filePath, 'utf-8'));
+    res.status(200).json(data);
+  } else if (req.method === 'POST') {
+    const newHero = req.body;
+    const data = JSON.parse(readFileSync(filePath, 'utf-8'));
+    data.push(newHero);
+    writeFileSync(filePath, JSON.stringify(data, null, 2));
+    res.status(201).json(newHero);
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
---
+++ src/components/api/id.js
@@ -0,0 +1,16 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+const filePath = join(process.cwd(), 'heroes.json');
+
+export default function handler(req, res) {
+  if (req.method === 'DELETE') {
+    const { id } = req.query;
+    const data = JSON.parse(readFileSync(filePath, 'utf-8'));
+    const updatedData = data.filter(hero => hero.id !== id);
+    writeFileSync(filePath, JSON.stringify(updatedData, null, 2));
+    res.status(200).json({ message: `Hero with id ${id} deleted.` });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
